7fcd7152b996030dc23c7bfa1e4e1eea352c3957
jongwan <jongbell4@gmail.com>
2025-03-24 10:37:45 +0900

    feat: PointService(interface)/ PointService*Test(class) 추가

---
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -0,0 +1,10 @@
+package io.hhplus.tdd.point;
+
+public interface PointService {
+
+    UserPoint charge(long id, long amount);
+
+    UserPoint use(long id, long amount);
+
+    UserPoint findById(long id);
+}
