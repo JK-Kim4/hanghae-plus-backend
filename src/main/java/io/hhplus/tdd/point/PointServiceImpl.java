fe9aef83cc6ec181f4962b85f30d93971e1c6efb
jongwan <jongbell4@gmail.com>
2025-03-24 12:16:01 +0900

    feat: PointServiceSelectTest 작성
    - 검증 테스트
      - 고유번호에 해당하는 UserPoint가 테이블에 존재하지않을 경우
      - 고유번호에 해당하는 UserPoint가 테이블에 존재할 경우

---
+++ src/main/java/io/hhplus/tdd/point/PointServiceImpl.java
@@ -0,0 +1,34 @@
+package io.hhplus.tdd.point;
+
+import io.hhplus.tdd.database.UserPointTable;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+@Component
+@RequiredArgsConstructor
+public class PointServiceImpl implements PointService {
+
+    private final Logger logger = LoggerFactory.getLogger(PointServiceImpl.class);
+
+    private final UserPointTable userPointTable;
+
+    @Override
+    public UserPoint charge(long id, long amount) {
+        return null;
+    }
+
+    @Override
+    public UserPoint use(long id, long amount) {
+        return null;
+    }
+
+    @Override
+    public UserPoint findById(long id) {
+        return userPointTable.selectById(id);
+    }
+
+}
---
+++ src/test/java/io/hhplus/tdd/point/PointServiceSelectTest.java
@@ -0,0 +1,51 @@
+package io.hhplus.tdd.point;
+
+import io.hhplus.tdd.database.UserPointTable;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class PointServiceSelectTest {
+
+    @DisplayName("전달받은 고유번호에 해당하는 UserPoint가 table에 존재하지 않을 경우 default 객체를 반환한다.")
+    @Test
+    void select_default_user_point_object_when_not_exist(
+            @Mock UserPointTable userPointTable
+    ) {
+        long notExistUserId = 999L;
+
+        when(userPointTable.selectById(notExistUserId))
+                .thenReturn(UserPoint.empty(notExistUserId));
+
+        PointService pointService = new PointServiceImpl(userPointTable);
+
+        UserPoint result = pointService.findById(notExistUserId);
+
+        Assertions.assertEquals(notExistUserId, result.id());
+        Assertions.assertEquals(0L, result.point());
+    }
+
+    @DisplayName("전달받은 고유번호에 해당하는 UserPoint객체가 table에 존재할 경우 해당 객체를 반환한다.")
+    @Test
+    void select_default_user_point_object_when_exist(
+            @Mock UserPointTable userPointTable
+    ){
+        UserPoint userPoint = new UserPoint(10L, 1000L, System.currentTimeMillis());
+
+        when(userPointTable.selectById(userPoint.id()))
+                .thenReturn(userPoint);
+
+        PointService pointService = new PointServiceImpl(userPointTable);
+
+        UserPoint result = pointService.findById(userPoint.id());
+
+        Assertions.assertEquals(userPoint.id(), result.id());
+        Assertions.assertEquals(userPoint.point(), result.point());
+    }
+}
